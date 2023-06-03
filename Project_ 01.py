5896a4a74eab550073f4ee8bbaabb2e9166d7d94
KeyvanAT1990 <133679587+KeyvanAT1990@users.noreply.github.com>
2023-06-03 20:21:13 +0330

    Add files via upload

---
+++ Project_ 01.py
@@ -0,0 +1,9 @@
+#Input number into number1
+number1=int(input('Enter Number1:'))
+number2=int(input('Enter Number2:'))
+
+if (number1>number2):
+    print(number1,'Azizam in adad bozorgtar ast az',number2)
+else:
+    print('number2 is bigger than number1')       
+
